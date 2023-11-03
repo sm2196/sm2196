79d8cce60e1ed91b415041268699c40fe743a51f
sm2196 <125245342+sm2196@users.noreply.github.com>
2023-11-03 10:26:44 +0400

    code formatted

--- Week 4/main.js
+++ Week 4/main.js
@@ -12,54 +12,54 @@ console.log(myCity[1].country); // Outputs: Dubai
 
 var myCities = [
     {
-    "name": "Dubai", 
-    "country": "UAE", 
-    "place": "Dubai Mall"
+        "name": "Dubai",
+        "country": "UAE",
+        "place": "Dubai Mall"
     },
     {
-    "name": "Milan", 
-    "country": "Italy", 
-    "place": "Duomo Di Milano"
+        "name": "Milan",
+        "country": "Italy",
+        "place": "Duomo Di Milano"
     }
 ];
 
 console.log(myCities[1].country); // Outputs: Italy
-    
+
 
 var cityContainer = document.getElementById("city-info");
 var btn = document.getElementById("btn");
-btn.addEventListener("click", function(){
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://sm2196.github.io/Week%204/cities1.json');
-ourRequest.onload = function() {
-var ourData = JSON.parse(ourRequest.responseText);
-renderHTML(ourData);
-btn.classList.add("hide-me");
-};
-ourRequest.send();
+btn.addEventListener("click", function () {
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://sm2196.github.io/Week%204/cities1.json');
+    ourRequest.onload = function () {
+        var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+        btn.classList.add("hide-me");
+    };
+    ourRequest.send();
 });
-function renderHTML(data){
-var htmlString = "";
-for (i=0; i<data.length; i++){
-htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + ",</br> Where you can enjoy indoor places like: " ;
-for (ii = 0; ii < data[i].places.indoor.length; ii++) {
-// Loop through the indoor places of the current city.
-if (ii == 0) {
-htmlString += data[i].places.indoor[ii];
-} else {
-htmlString += ", and " + data[i].places.indoor[ii];
-}
-}
-htmlString += '. & enjoy outdoor places like: ';
-// Loop through the outdoor places of the current city.
-for (ii = 0; ii < data[i].places.outdoor.length; ii++) {
-if (ii == 0) {
-htmlString += data[i].places.outdoor[ii];
-} else {
-htmlString += " and " + data[i].places.outdoor[ii];
-}
-}
-htmlString += '.</p>';
-}
-cityContainer.insertAdjacentHTML('beforeend' , htmlString);
+function renderHTML(data) {
+    var htmlString = "";
+    for (i = 0; i < data.length; i++) {
+        htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + ",</br> Where you can enjoy indoor places like: ";
+        for (ii = 0; ii < data[i].places.indoor.length; ii++) {
+            // Loop through the indoor places of the current city.
+            if (ii == 0) {
+                htmlString += data[i].places.indoor[ii];
+            } else {
+                htmlString += ", and " + data[i].places.indoor[ii];
+            }
+        }
+        htmlString += '. & enjoy outdoor places like: ';
+        // Loop through the outdoor places of the current city.
+        for (ii = 0; ii < data[i].places.outdoor.length; ii++) {
+            if (ii == 0) {
+                htmlString += data[i].places.outdoor[ii];
+            } else {
+                htmlString += " and " + data[i].places.outdoor[ii];
+            }
+        }
+        htmlString += '.</p>';
+    }
+    cityContainer.insertAdjacentHTML('beforeend', htmlString);
 }
