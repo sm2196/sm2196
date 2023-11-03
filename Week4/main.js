d1961839435ea5071b220f2014c5ef93271dcbb2
sm2196 <125245342+sm2196@users.noreply.github.com>
2023-11-03 10:40:36 +0400

    code

--- Week4/main.js
+++ Week4/main.js
@@ -30,7 +30,7 @@ var cityContainer = document.getElementById("city-info");
 var btn = document.getElementById("btn");
 btn.addEventListener("click", function () {
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://sm2196.github.io/Week4/cities1.json');
+    ourRequest.open('GET', 'https://sm2196.github.io/sm2196/Week4/cities1.json');
     ourRequest.onload = function () {
         var ourData = JSON.parse(ourRequest.responseText);
         renderHTML(ourData);
